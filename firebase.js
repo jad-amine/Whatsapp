f0722a9fc4675c78090e5fb0fdcd90e62fd9abd9
jad amine <jade1992amine@gmail.com>
2022-07-17 09:36:56 +0300

    Added firebase auth functions

--- firebase.js
+++ firebase.js
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA-ijSTmGKKxLvJmFNjdeZsPAaXXj3K6tQ",
@@ -10,4 +13,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = initializeFirestore(app, { experimentalForceLongPolling });
