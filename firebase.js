8363a3b9bbdd6c149a5c525f70e22c90eb89aec6
jad amine <jade1992amine@gmail.com>
2022-07-17 09:30:37 +0300

    Added Firebase Backend Services

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-ijSTmGKKxLvJmFNjdeZsPAaXXj3K6tQ",
+  authDomain: "whatsapp-bc767.firebaseapp.com",
+  projectId: "whatsapp-bc767",
+  storageBucket: "whatsapp-bc767.appspot.com",
+  messagingSenderId: "246809012285",
+  appId: "1:246809012285:web:f57f6e3f2bdeaa58d2e254",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
